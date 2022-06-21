6cadd8cce33c30a3391cce09912acf19cbedbba1
Danielle Alves Feih <89142483+DanielleAlves17@users.noreply.github.com>
2022-06-21 19:03:21 -0300

    Add files via upload

---
+++ CodeGirl/Aula 09/Aula09_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula09_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 02. Calculadora
+            //Já criamos uma calculadora anteriormente, agora, vamos modificá-la aplicando os conceitos aprendidos
+            //até aqui. Crie uma classe calculadora. Esta classe deve ser abstrata e implementar as operações básicas
+            //(soma, subtração, divisão e multiplicação).
+            //Utilizando o conceito de herança crie uma classe chamada calculadora científica que implementa os seguintes
+            //cálculos: raiz quadrada e a potência.
+            //Utilize sobrecarga para aceitar valores inteiros e reais em seus cálculos.
+            //Dica: Utilize a biblioteca Math para facilitar seus cálculos.
+            //Na classe main você deve instanciar a calculadora e ao executar o programa, você deve ser capaz de
+            //selecionar a operação desejada e inserir os valores a serem calculados.
+
+        }
+    }
+}
---
+++ CodeGirl/Aula 09/Aula09_03/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula09_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 - Veículos
+            //Criar um programa que representa os veículos do mundo real como uma classe abstrata.
+            //Também escolha dois veículos para herdá-la, como por exemplo Bicicleta e Carro.
+            //Os métodos da classe Veiculo são todos abstratos e devem corresponder a ação de acelerar e frear.
+            //Estes métodos são implementados nas Carro e Bicicleta, por exemplo.
+            //Acrescentar nas classes derivadas, métodos específicos tais como trocar óleo para classe carro,
+            //ajustar a correia para classe bicicleta.
+            //Na classe main instancie os veículos e utilize seus métodos.
+        }
+    }
+}
